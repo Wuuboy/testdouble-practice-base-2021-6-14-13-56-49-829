88ed071e89bdbbdf1839dfe9883a5e3ae4962cf2
Demi Cao <demi.cao@oocl.com>
2021-06-16 21:00:11 +0800

    [Demi] - add test case for task1

--- src/test/java/com/tw/comprehensive/TradingServiceTest.java
+++ src/test/java/com/tw/comprehensive/TradingServiceTest.java
@@ -1,7 +1,26 @@
 package com.tw.comprehensive;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class TradingServiceTest {
+
+    @Test
+    void should_call_logNewTrade_when_createTrade() {
+        //given
+        AuditService auditService = spy(AuditService.class);
+        TradeRepository tradeRepository = spy(TradeRepository.class);
+        TradingService tradingService = new TradingService(tradeRepository, auditService);
+
+        //when
+        tradingService.createTrade(new Trade("test", "test"));
+
+        //then
+        verify(auditService, times(1)).logNewTrade(any());
+    }
+
 
 }
